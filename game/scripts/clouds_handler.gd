0639f273aaee8efd0a4246349c95dd9efe799c64
evrardco <colin.evrard.134@gmail.com>
2025-01-25 13:23:19 +0100

    rotated the scene

--- game/scripts/clouds_handler.gd
+++ game/scripts/clouds_handler.gd
@@ -1,6 +1,5 @@
-extends Node3D
-
-
+extends MeshInstance3D
+@onready var area : BoxMesh = self.mesh
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
