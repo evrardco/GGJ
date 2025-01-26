d6462ec736801612b32695ae2761760f4db4b3b1
Baxdy <paluch.baptiste@gmail.com>
2025-01-26 09:08:17 +0100

    Menu et cinématiques

--- menu.gd
+++ menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://game/scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Cinématique de partie.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
